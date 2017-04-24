37781222145ad9782aa0a4e96a9abbddb2729a76
Juan van Vuuren <juan@coltech.co.za>
2017-04-24 21:54:49 +0200

    First commit

---
+++ main.go
@@ -0,0 +1,17 @@
+// A basic hello world example using go.
+// Input: This program doesn't require any input 
+//   from the user. 
+// Output: It sends the output to the console.
+
+package main
+
+import (
+	"fmt"
+)
+
+// Output to console
+func main() {
+	// body
+	fmt.Printf("hello, world\n")
+}
+
